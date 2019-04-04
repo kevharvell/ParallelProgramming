1ded1840b3d5bb8078a8bcb188c3cc11fb8e557e
Kevin Harvell <kevharvell@gmail.com>
2019-04-03 21:18:03 -0500

    fix script

--- project0/project0/script.sh
+++ project0/project0/script.sh
@@ -7,7 +7,7 @@ do
 	for s in 2 4 8 16 32 64 128 256 512 1024 2048 3072 4096
 	do
 		echo ARRAYSIZE = $s
-		g++ -DARRAYSIZE=$s -DNUMT=$t project0.cpp -o project0 -lm -fopenmp
+		/usr/local/common/gcc-8.2.0/bin/g++ -DARRAYSIZE=$s -DNUMT=$t project0.cpp -o project0 -lm -fopenmp
 		./project0
 	done
 done
