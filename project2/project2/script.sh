84314543d09402dac614b02f543f4b2feceef401
Kevin Harvell <kevharvell@gmail.com>
2019-04-20 12:10:33 -0500

    updated script numbers

--- project2/project2/script.sh
+++ project2/project2/script.sh
@@ -4,7 +4,7 @@ for t in 1 2 4 6 8
 do
 	echo NUMT = $t
 	# number of subdivisions:
-	for s in 100 1000 10000 20000 40000 80000 160000 320000 640000 1280000
+	for s in 100 200 400 800 1600 3200 6400
 	do
 		echo NUMNODES = $s
 		/usr/local/common/gcc-8.2.0/bin/g++ -DNUMNODES=$s -DNUMT=$t project2.cpp -o project2 -lm -fopenmp
