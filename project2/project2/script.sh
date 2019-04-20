5ec61b8245360d1778c71a3e5313aef6710c32ac
Kevin Harvell <kevharvell@gmail.com>
2019-04-20 12:04:34 -0500

    update script

--- project2/project2/script.sh
+++ project2/project2/script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # number of threads:
-for t in 1 2 4 6 8 10 12 14 16 18 20
+for t in 1 2 4 6 8
 do
 	echo NUMT = $t
 	# number of subdivisions:
-	for s in 100000 200000 400000 800000 1600000 3200000 6400000 12800000
+	for s in 100 1000 10000 20000 40000 80000 160000 320000 640000 1280000
 	do
-		echo NUMTRIALS = $s
-		/usr/local/common/gcc-8.2.0/bin/g++ -DNUMTRIALS=$s -DNUMT=$t project1.cpp -o project1 -lm -fopenmp
-		./project1
+		echo NUMNODES = $s
+		/usr/local/common/gcc-8.2.0/bin/g++ -DNUMNODES=$s -DNUMT=$t project2.cpp -o project2 -lm -fopenmp
+		./project2
 	done
 done
