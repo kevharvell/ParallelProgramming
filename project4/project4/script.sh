6d9363b85e15695c84678a9921ff7ff3fa8be553
Kevin Harvell <kevharvell@gmail.com>
2019-05-07 15:00:58 -0500

    makefile

--- project4/project4/script.sh
+++ project4/project4/script.sh
@@ -3,6 +3,6 @@
 for s in 1000 2000 4000 8000 16000 32000 64000 128000 256000 512000 1024000 2048000 4096000 8192000
 do
 	echo ARRAY_SIZE = $s
-	g++ -DARRAY_SIZE=$s - o project4  project4.cpp simd.p4.o  -lm  -fopenmp
+	g++ -DARRAY_SIZE=$s -o project4  project4.cpp simd.p4.o  -lm  -fopenmp
 	./project4
 done
