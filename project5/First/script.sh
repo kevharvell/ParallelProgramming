1612268d9e205c0c088c2b61935c25f0982622b1
Kevin Harvell <kevharvell@gmail.com>
2019-05-14 08:45:23 -0500

    edit script

--- project5/First/script.sh
+++ project5/First/script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # array sizes
-for s in 1024 2048
+for s in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
 do
-	echo NUM_ELEMENTS = $s
-	g++ -DNUM_ELEMENTS=$s -o first first.cpp /usr/lib/x86_64-linux-gnu/libOpenCL.so -lm  -fopenmp
-	./first
+	for t in 8 16 32 64 128 256 512
+	do
+		echo NUM_ELEMENTS = $s
+		echo LOCAL_SIZE = $t
+		g++ -o first first.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
+		./first
+	done
 done
