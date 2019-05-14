4a52e6b3d8c5349a87dd2828e693d1ec33d73d89
Kevin Harvell <kevharvell@gmail.com>
2019-05-14 15:19:43 -0500

    new script for second experiment

---
+++ project5/First/script2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# array sizes
+for s in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
+do
+	for t in 8 16 32 64 128 256 512
+	do
+		echo NUM_ELEMENTS = $s
+		echo LOCAL_SIZE = $t
+		g++ -DNUM_ELEMENTS=$s -DLOCAL_SIZE=$t -o second second.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
+		./second
+	done
+done
