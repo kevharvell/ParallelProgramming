4d2a3754c446c3a544ec4f4cc4015538591a2de5
Kevin Harvell <kevharvell@gmail.com>
2019-05-14 17:38:09 -0500

    commenting out check code

--- project5/First/second.cpp
+++ project5/First/second.cpp
@@ -222,7 +222,7 @@ main(int argc, char *argv[])
 
 	// did it work?
 
-	for (int i = 0; i < NUM_ELEMENTS; i++)
+	/*for (int i = 0; i < NUM_ELEMENTS; i++)
 	{
 		float expected = hA[i] * hB[i] + hC[i];
 		if (fabs(hD[i] - expected) > TOL)
@@ -233,7 +233,7 @@ main(int argc, char *argv[])
 				i, LookAtTheBits(hA[i]), LookAtTheBits(hB[i]), LookAtTheBits(hC[i]), LookAtTheBits(expected));
 		}
 	}
-
+*/
 	fprintf(stderr, "%8d\t%4d\t%10d\t%10.3lf GigaMultsPerSecond\n",
 		NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS, (double)NUM_ELEMENTS / (time1 - time0) / 1000000000.);
 
